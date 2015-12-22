f1623d4c8b45a3da0e809a06da805c49043e69b1
Maksym Shkolnyi <mshkolnyi@intropro.com>
2015-12-22 17:23:56 +0200

    Sort fix 1

--- src/main/java/ua/pp/msk/julia/my/tasktodo/test/chapter6/SortNumbers.java
+++ src/main/java/ua/pp/msk/julia/my/tasktodo/test/chapter6/SortNumbers.java
@@ -14,16 +14,14 @@ public class SortNumbers {
     public static void sort(double[] nums) {
 
         for (int i = 0; i < nums.length; i++) {
-            int min = i;
+            
 
-            for (int j = i; j < nums.length; j++) {
-                if (nums[j] < nums[min]) {
-                    min = j;
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[j] < nums[i]) {
+                double tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
                 }
-                double tmp;
-                tmp = nums[i];
-                nums[i] = nums[min];
-                nums[min] = tmp;
             }
         }
     }
@@ -38,5 +36,6 @@ public class SortNumbers {
         for (int i = 0; i < nums.length; i++) {
             System.out.println(nums[i]);
         }
+        System.out.println("Minimal value is :" + nums[0]);
     }
 }
